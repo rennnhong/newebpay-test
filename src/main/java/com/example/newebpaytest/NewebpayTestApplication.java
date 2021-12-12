8bc6a0025e244a60eda57d328f6444aea29dbdc2
kdcc-005 <ray.luo@kangdainfo.com>
2021-12-12 22:40:19 +0800

    update

--- src/main/java/com/example/newebpaytest/NewebpayTestApplication.java
+++ src/main/java/com/example/newebpaytest/NewebpayTestApplication.java
@@ -2,8 +2,10 @@ package com.example.newebpaytest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class NewebpayTestApplication {
 
     public static void main(String[] args) {
